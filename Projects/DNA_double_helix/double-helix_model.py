81fbafeaec4687561ab00e4f13bfe5d6480ffa76
andremr <andremrsantos@gmail.com>
2015-11-24 11:35:47 -0800

    Changed DNA-Helix rotation, included `nucleotide` arrows

--- Projects/DNA_double_helix/double-helix_model.py
+++ Projects/DNA_double_helix/double-helix_model.py
@@ -4,19 +4,35 @@ import numpy as np
 import matplotlib.pyplot as pl
 
 
+# In[115]:
+
 # Parameters
 a = 1.0
 b = 0.8
-wavelength   = 1.   # length of each turn
-Nturns       = 5    # number of helices' turns
-Npoints      = 1e3  # number of points in the curves
 
+wavelength   = 1.   # length of each turn
+Nturns       = 20   # number of helices' turns
+Npoints      = 1e2  # number of points in the curves
+
+
+# In[116]:
+
+def degrad(x):
+    return (x / 180. * np.pi)
 
 # Derived parameters
 t = np.linspace(0, wavelength*Nturns, int(Npoints) )
-omega = 2*np.pi/wavelength
-theta = wavelength*1.2
+omega = degrad(32.5)/wavelength
+theta = wavelength * 1.5
+skip  = int(Npoints/Nturns)
 
+
+# In[117]:
+
+colors = np.random.choice(['red', 'yellow', 'blue', 'black'], Nturns)
+
+
+# In[118]:
 
 # Curves
 x1 = a*np.sin(omega*t)
@@ -30,9 +46,15 @@ y2 = b*np.cos(omega*t + theta)
 fig = pl.figure()
 ax = fig.gca(projection='3d')
 ax.plot(x1, y1, t, '-b', lw=4)
-ax.plot(x2, y2, t, '-b', lw=4)
+ax.plot(x2, y2, t, '-r', lw=4)
+
+rg = np.arange(Nturns) * skip
+dx = (x1 - x2)[rg]
+dy = (y1 - y2)[rg]
+ax.quiver(x1[rg], y1[rg], t[rg], -dx, -dy, 0, length=.5, color=colors, pivot='tail', lw=3)
+ax.quiver(x2[rg], y2[rg], t[rg],  dx,  dy, 0, length=.5, color=colors, pivot='tail', lw=3)
 ax.set_xlim([-3,3])
 ax.set_ylim([-3,3])
 
+# For some reason, the red curve is always plotted in front of the blue
 pl.show()
-
